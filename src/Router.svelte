114dc05d1407974b3b629c9a49aafb81725de4fc
tuxer <e.yulian@gmail.com>
2020-09-25 16:28:23 +0700

    basic routing

---
+++ src/Router.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+  import router from "page";
+  import Home from "./Home.svelte";
+
+  let page;
+
+  export function Page() {
+    return page;
+  }
+
+  router("/", () => router.redirect("/home"));
+  router("/home", () => (page = Home));
+
+  router.start();
+</script>
