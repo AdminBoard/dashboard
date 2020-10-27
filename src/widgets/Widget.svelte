140949c00e8e36bd0ed4a75ef4f05da8929d3acf
tuxer <e.yulian@gmail.com>
2020-10-27 20:56:04 +0700

    remove widget session

--- src/widgets/Widget.svelte
+++ src/widgets/Widget.svelte
@@ -6,26 +6,11 @@
     export let data = null;
 
     let dispatch = createEventDispatcher();
-
-    const session = {
-        set: (key, value) => {
-            sessionStorage.setItem(
-                "widget." + content.id + "." + key,
-                JSON.stringify(value)
-            );
-        },
-        get: (key) => {
-            return JSON.parse(
-                sessionStorage.getItem("widget." + content.id + "." + key)
-            );
-        },
-    };
 </script>
 
 {#if content != null}
     <svelte:component
         this={getWidget(content.name)}
-        {session}
         {data}
         properties={content.properties}
         on:select={(ev) => dispatch('action', ev.detail)} />
