3c64845ecdfd8ddd50f754a3aaed5445712ac2ac
tuxer <e.yulian@gmail.com>
2021-06-17 23:09:16 +0700

    add layout and snackbar

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import "../styles/app.scss";
+
+	import Snackbar, { Actions, Label } from "@smui/snackbar";
+	import IconButton from "@smui/icon-button";
+	import snackbar from "$lib/snackbar";
+
+	let snackbarWithClose;
+	let snackbarMsg;
+
+	snackbar.notify = (msg) => {
+		snackbarMsg = msg;
+		snackbarWithClose.open();
+	};
+</script>
+
+<main>
+	<slot />
+
+	noti
+
+	<Snackbar bind:this={snackbarWithClose}>
+		<Label>{snackbarMsg}</Label>
+		<Actions>
+			<IconButton class="material-icons" title="Dismiss">
+				close
+			</IconButton>
+		</Actions>
+	</Snackbar>
+</main>
